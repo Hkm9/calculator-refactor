24aab0f9856569dd88d42ffcf6c30d749f275a4d
Hakim <hakim.mame96@gmail.com>
2020-02-03 21:22:03 +0100

    This is the logic.js
    this is responsible for the calculation process in this web page.

--- scripts/listener.js
+++ scripts/listener.js
@@ -1,6 +1,18 @@
+//listener.js
 function clac(){
   var a = parseInt(document.querySelector("#value1").value);
   var b = parseInt(document.querySelector("#value2").value);
   var op = document.querySelector("#operator").value;
   var Calculate;
+
+//logic.js
+  if (op == "add"){
+    Calculate = a + b;
+  } else if (op == "min"){
+    Calculate = a - b;
+  } else if (op == "div"){
+    Calculate = a / b;
+  }else if (op == "mul"){
+    Calculate = a * b;
+  }
 }
