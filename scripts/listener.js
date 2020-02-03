53d70aa6458378c645199864a116744fdf40025d
Hakim <hakim.mame96@gmail.com>
2020-02-03 22:14:56 +0100

    This is the handler js
    this is the js which will display the result for the user and some css added to the html

--- scripts/listener.js
+++ scripts/listener.js
@@ -1,6 +1,20 @@
-function clac(){
+//listener.js
+function calc(){
   var a = parseInt(document.querySelector("#value1").value);
   var b = parseInt(document.querySelector("#value2").value);
   var op = document.querySelector("#operator").value;
   var Calculate;
+
+//logic.js
+  if (op == "add"){
+    Calculate = a + b;
+  } else if (op == "min"){
+    Calculate = a - b;
+  } else if (op == "div"){
+    Calculate = a / b;
+  }else if (op == "mul"){
+    Calculate = a * b;
+  }
+//handler.js
+  document.querySelector("#result").innerHTML = Calculate;
 }
